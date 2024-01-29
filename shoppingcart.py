3ed8a1e15ba4cd79c74fc425c94a565a4f10711a
Brad Nielsen <brad_nielsen29@hotmail.com>
2024-01-29 10:09:28 +1000

    commit

--- shoppingcart.py
+++ shoppingcart.py
@@ -8,12 +8,20 @@ num_of_apples = 20 #integer
 message = "Cost is equal: " #string 
 out_of_money = False #Boolean (yes or no) 0, or 1
 
+print(type(money))
+print(type(apple))
+print(type(num_of_apples))
+print(type(message))
+print(type(out_of_money))
+
 #VARIABLES ARE STORED IN MEMORY
 #NAME error - means you referred to a variable that does not exit
+
+name = input("What is your name")
+print("Hello " + name)
 
 num_of_apples = input("How many apples: ")
 cost = num_of_apples*apple
 
-#TYPE error - means you referred to a variable by the wrong datatype
-print(message + str(cost))
+print(message + cost)
 
